ef9105a1f6767ffc21618aab1570e1008e7507ef
Andrew Schenck <omnova@gmail.com>
2018-09-16 00:26:07 -0500

    Added more settings.

--- Shackmeets/AppSettings.cs
+++ Shackmeets/AppSettings.cs
@@ -7,7 +7,27 @@ namespace Shackmeets
 {
   public class AppSettings
   {
+    public class ChattySettings
+    {
+      public string Username { get; set; }
+      public string Password { get; set; }
+    }
+
+    public class GoogleMapsSettings
+    {
+      public string MapsApiKey { get; set; }
+    }
+
     public string Secret { get; set; }
     public string ConnectionString { get; set; }
+
+    public ChattySettings Chatty { get; set; }
+    public GoogleMapsSettings GoogleMaps { get; set; }
+
+    public AppSettings()
+    {
+      this.Chatty = new ChattySettings();
+      this.GoogleMaps = new GoogleMapsSettings();
+    }
   }
 }
